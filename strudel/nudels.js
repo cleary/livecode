02201b46ebede78d260210a8f3993858b131eb38
Bernard Gray <bernard.gray@gmail.com>
2025-10-09 06:22:38 +1100

    add some synth stuff

--- strudel/nudels.js
+++ strudel/nudels.js
@@ -8,3 +8,31 @@ samples('github:tidalcycles/dirt-samples')
 $: s("breaks165")
 .splice(8, "<[2 1 1 4] [6 2 3 5] [2 1 1 4] [4 4 2 3]>*2")
 .gain(1.2).ribbon("0 .25", 2).almostNever(ply("2"))
+
+// lead synth tones 
+// sawtooth w/some breakup
+  .s("sawtooth")
+  .fm("<0 1 2 8 32>*8")
+  .fmh("<1 2>")
+  .fmattack(0.05)
+  .fmdecay(0.5)
+  .fmsustain(0.1)
+  .fmenv("<exp lin>")
+  .prelease(-0.05)
+  .penv(2)
+  .pcurve("exp")
+
+// bass synth tones
+// mid-heavy with some breakup 
+  .s("square")
+  .transpose(-24)
+  .lpf(800)
+  .fm("<0 1 2 8 32>*8") //control breakup here
+  .fmh("<1 2>")
+  .fmattack(0.05)
+  .fmdecay(0.5)
+  .fmsustain(0.1)
+  .fmenv("<exp lin>")
+  .prelease(-0.05)
+  .penv(2)
+  .pcurve("exp")
