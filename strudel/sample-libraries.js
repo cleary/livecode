638bbe3bbf30956170d0816a192ce4a799d073b7
Bernard Gray <bernard.gray@gmail.com>
2025-10-26 04:20:20 +1100

    vet akas_dnb_essentials

--- strudel/sample-libraries.js
+++ strudel/sample-libraries.js
@@ -6,7 +6,10 @@ samples('https://samples.grbt.com.au/strudel.json')
 // dirt-samples
 samples('github:tidalcycles/dirt-samples')
 
+// pads_drones(16), bass_one_shots(24), sfx(24)
+samples('github:0nefeather/akas-dnb-essentials') //20251012
+
+
 // unsorted/tested
 samples('github:bubobubobubobubo/dough-waveforms')
 samples('github:yaxu/clean-breaks')
-samples('github:0nefeather/akas-dnb-essentials') //20251012
